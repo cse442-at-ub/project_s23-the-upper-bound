d0271864ed58594ec17d5d68bbc07bb49348ecca
eabock <eabock29@gmail.com>
2023-03-15 03:25:56 -0400

    added buttons for some resource, including mental health, #36

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -112,11 +112,24 @@ export default function HomeScreen({ navigation }) {
 							title="Food"
 							onPress={() => navigation.navigate("events")}
 						>
-							<Text style={styles.buttons}>📃</Text>
+							<Text style={styles.buttons}>📰</Text>
 						</Pressable>
 						<Text style={styles.underButtonText}>Events</Text>
 					</View>
+
+					<View style={styles.buttonAndTextViewRow3}>
+						<Pressable
+							style={styles.closerMapButtons}
+							title="Resource"
+							onPress={() => navigation.navigate("resources")}
+						>
+							<Text style={styles.buttons}>📖</Text>
+						</Pressable>
+						<Text style={styles.underButtonText}>Resources</Text>
+					</View>
 				</View>
+
+				
 
 				<ImageBackground
 					source={require("../assets/WelBot.png")}
--- Components/ResourceScreen.js
+++ Components/ResourceScreen.js
@@ -7,13 +7,103 @@ import * as WebBrowser from 'expo-web-browser';
 
 export default function ResourceScreen({navigation}){
     return (
-        <View>
+        <View style={styles.container}>
+        <ImageBackground
+				source={require("../assets/BlueTri.png")}
+				style={styles.welcomeUp}
+		></ImageBackground>
+        <Text style={styles.userTxt}>Important Resources</Text> 
+        <Pressable
+            style={styles.MHbuttons}
+            title="Mental Health"
+            onPress={() => Linking.openURL('https://www.buffalo.edu/studentlife/life-on-campus/health/mental-well-being/counseling.html')}>
+            <Text style={styles.buttonTXT}>Mental Health</Text>
+            </Pressable>
+
+            <Pressable
+            style={styles.Mbuttons}
+            title="MEDICAL"
+            onPress={() => Linking.openURL('https://www.buffalo.edu/studentlife/life-on-campus/health/medical-care.html')}>
+            <Text style={styles.buttonTXT}>Medical</Text>
+            </Pressable>
+
+            <Pressable
+            style={styles.CLbuttons}
+            title="MEDICAL"
+            onPress={() => Linking.openURL('https://www.buffalo.edu/campusliving.html')}>
+            <Text style={styles.buttonTXT}>Campus Living</Text>
+            </Pressable>
 
 
-            
+
+
+
+
+
         </View>
 
 
 
-    )
+
+
+    );
 }
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1E1E1E",
+    alignItems: "center",
+  },
+  userTxt: {
+    color: "#FFFFFF",
+    marginLeft: 10,
+    fontSize: 40,
+    top: -120,
+  },
+  welcomeUp: {
+    height: 200,
+		width: 300,
+		marginLeft: -100,
+        justifyContent:"center",
+        transform: [{ scaleX: -1 }]
+  },
+  MHbuttons: {
+    height:100,
+    width: 100,
+    justifyContent: 'center',
+    alignItems:'center',
+    top: -30,
+    left:-80,
+    backgroundColor: "#3265CB",
+    borderRadius:30,
+  },
+  Mbuttons: {
+    height:100,
+    width: 100,
+    justifyContent: 'center',
+    alignItems:'center',
+    top: -130,
+    left:80,
+    backgroundColor: "#3265CB",
+    borderRadius:30,
+  },
+  CLbuttons: {
+    height:100,
+    width: 100,
+    justifyContent: 'center',
+    alignItems:'center',
+    top: -100,
+    left:-80,
+    backgroundColor: "#3265CB",
+    borderRadius:30,
+  },
+  buttonTXT: {
+    fontSize: 20,
+    color:"white",
+    borderRadius:10,
+  },
+});
