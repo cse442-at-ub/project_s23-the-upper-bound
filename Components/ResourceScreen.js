80e3d80df76ec9202562d7dcca785ce575f3594c
eabock <eabock29@gmail.com>
2023-03-15 02:38:29 -0400

    created file for Resources Screen, #35

---
+++ Components/ResourceScreen.js
@@ -0,0 +1,19 @@
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View, ImageBackground} from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Linking from 'expo-linking';
+import * as WebBrowser from 'expo-web-browser';
+
+
+export default function ResourceScreen({navigation}){
+    return (
+        <View>
+
+
+            
+        </View>
+
+
+
+    )
+}
