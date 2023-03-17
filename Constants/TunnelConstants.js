986ec0f5abe3a61d6d25bb54d480896127059a66
gigiboss56 <35941351+MsKitty1@users.noreply.github.com>
2023-03-16 20:31:54 -0400

    Removed problematic coordinates for oBrianHall polygon #32

--- Constants/TunnelConstants.js
+++ Constants/TunnelConstants.js
@@ -1,5 +1,11 @@
 // All coordinates for markers and polygons go in this file
 
+export const MapBoundary = { 
+    boundaries: {
+    northEast: { latitude: 43.00369, longitude: -78.77989 },
+    southWest: { latitude: 42.99900, longitude: -78.79405 }}
+}
+
 export const oBrianHallMarker = { latitude: 43.000418729480515, longitude: -78.78794125293997 }
 export const oBrianHall = {
     coordinates: [
@@ -7,32 +13,23 @@ export const oBrianHall = {
         { name2: '2', latitude: 43.000282530596685, longitude: -78.78847511063657 },
         { name3: '3', latitude: 43.00026879915413, longitude: -78.78847511063657 },
         { name4: '4', latitude: 43.00027409357842, longitude: -78.78774973766855 },
-        { name5: '5', latitude: 43.00028446379197, longitude: -78.78774771201974 },
-        { name6: '6', latitude: 43.00028396997234, longitude: -78.78773758377561 },
-        { name7: '7', latitude: 43.00026174808382, longitude: -78.78773690855934 },
-        { name8: '8', latitude: 43.000260760444164, longitude: -78.78770922468854 },
-        { name9: '9', latitude: 43.00026597742664, longitude: -78.78770797868587 },
-        { name10: '10', latitude: 43.000260760444164, longitude: -78.78770922468854 },
-        { name11: '11', latitude: 43.00026668336849, longitude: -78.78762107881445 },
-        { name12: '12', latitude: 43.000294636660016, longitude: -78.78762241991889 },
-        { name13: '13', latitude: 43.00029512706852, longitude: -78.78757883402464 },
-        { name14: '14', latitude: 43.000352995245194, longitude: -78.78757950457687 },
-        { name15: '15', latitude: 43.00035495687736, longitude:  -78.78749903831054 },
-        { name16: '16', latitude: 43.00035495687736, longitude:  -78.78749903831054 },
-        { name17: '17', latitude: 43.00056436075405, longitude:  -78.78750574382855 },
-        { name18: '18', latitude: 43.000563870347676, longitude:  -78.78758889230373 },
-        { name19: '19', latitude: 43.00053346514568, longitude:  -78.78759492727372 },
-        { name20: '20', latitude: 43.00053297473908, longitude:  -78.78766198249565 },
-        { name21: '21', latitude: 43.00063301760722, longitude:  -78.78766332361388 },
-        { name22: '22', latitude: 43.000631654532405, longitude:  -78.78822312243344 },
-        { name23: '23', latitude: 43.00050709132743, longitude:  -78.788221781329 },
-        { name24: '24', latitude: 43.00050611051381, longitude:  -78.7883123058786 },
-        { name25: '25', latitude: 43.00048208057474, longitude:  -78.7883116353264 },
-        { name26: '26', latitude: 43.000481099760684, longitude: -78.7883880782794 },
-        { name27: '27', latitude: 43.00043647270535, longitude: -78.78838874883161 },
-        { name28: '28', latitude: 43.0004359822975, longitude: -78.78847592062327 },
-        { name29: '29', latitude: 43.00035359380021, longitude: -78.78847457952168 },
-        { name30: '30', latitude: 43.000352612984116, longitude: -78.78851481265484 },
+        { name14: '5', latitude: 43.000352995245194, longitude: -78.78757950457687 },
+        { name15: '6', latitude: 43.00035495687736, longitude:  -78.78749903831054 },
+        { name16: '7', latitude: 43.00035495687736, longitude:  -78.78749903831054 },
+        { name17: '8', latitude: 43.00056436075405, longitude:  -78.78750574382855 },
+        { name18: '9', latitude: 43.000563870347676, longitude:  -78.78758889230373 },
+        { name19: '10', latitude: 43.00053346514568, longitude:  -78.78759492727372 },
+        { name20: '11', latitude: 43.00053297473908, longitude:  -78.78766198249565 },
+        { name21: '12', latitude: 43.00063301760722, longitude:  -78.78766332361388 },
+        { name22: '13', latitude: 43.000631654532405, longitude:  -78.78822312243344 },
+        { name23: '14', latitude: 43.00050709132743, longitude:  -78.788221781329 },
+        { name24: '15', latitude: 43.00050611051381, longitude:  -78.7883123058786 },
+        { name25: '16', latitude: 43.00048208057474, longitude:  -78.7883116353264 },
+        { name26: '17', latitude: 43.000481099760684, longitude: -78.7883880782794 },
+        { name27: '18', latitude: 43.00043647270535, longitude: -78.78838874883161 },
+        { name28: '19', latitude: 43.0004359822975, longitude: -78.78847592062327 },
+        { name29: '20', latitude: 43.00035359380021, longitude: -78.78847457952168 },
+        { name30: '21', latitude: 43.000352612984116, longitude: -78.78851481265484 },
     ]
 }
 
